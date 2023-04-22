a0a078be7c4b9b97342a58bb254bf9ad4686089c
sachinmagadum <sachinmagadum5009@gmail.com>
2023-04-22 10:18:59 +0530

    code for sub

--- arithmatic.py
+++ arithmatic.py
@@ -2,7 +2,14 @@ def add(a,b):
     return a+b
 
 
-a = 10
-b = 20
+def sub(a, b):
+    return a-b
+
+
+a = 20
+b = 10
 ans = add(a,b)
 print(f"Addition of {a} and {b} is {ans}")
+
+ans = sub(a,b)
+print(f"Subtraction of {a} and {b} is {ans}")
