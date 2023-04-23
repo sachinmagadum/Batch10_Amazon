6fa6803c5951c8c00d51b8031c4f5752b5452c25
sachinmagadum <sachinmagadum5009@gmail.com>
2023-04-23 10:18:28 +0530

    div code

--- arithmatic.py
+++ arithmatic.py
@@ -13,3 +13,9 @@ print(f"Addition of {no1} and {no2} is {ans}")
 
 ans = sub(no1, no2)
 print(f"Subtraction of {no1} and {no2} is {ans}")
+
+def div(num1, num2):
+    return num1 / num2
+
+ans = div(no1, no2)
+print(f"Division of {no1} and {no2} is {ans}")
